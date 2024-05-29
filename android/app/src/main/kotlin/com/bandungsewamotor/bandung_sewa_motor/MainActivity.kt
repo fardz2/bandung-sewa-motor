ae6117c2b7d7f5db71443b2e66fd98490201b846
Muhammad Faridz <faridzmuhamad679@gmail.com>
2024-05-29 20:53:05 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/bandungsewamotor/bandung_sewa_motor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bandungsewamotor.bandung_sewa_motor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
+  FlutterNativeSplash.remove();
+}
