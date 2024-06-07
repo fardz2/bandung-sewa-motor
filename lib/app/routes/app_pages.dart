00df6a43b41950d9ba5af61d74c786dc973508b2
Muhammad Faridz <faridzmuhamad679@gmail.com>
2024-06-07 16:16:41 +0700

    mengganti route initial

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -40,7 +40,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.LANDING;
 
   static final routes = [
     GetPage(
